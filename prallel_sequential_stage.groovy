702de18d4798615b43aa11bf52795d220bd7a413
Devi <16388989+dkhode@users.noreply.github.com>
2019-07-15 14:30:50 -0700

    Add sequential stages with in a parallel stage

---
+++ prallel_sequential_stage.groovy
@@ -0,0 +1,45 @@
+#!groovy
+
+pipeline {
+    agent none
+
+    stages {
+        stage ("Stage1") {
+            parallel {
+                stage ("parallel1") {
+                    agent { label "build" }
+                    stages {
+                        stage ("parallel1 stage1") {
+                            steps {
+                                echo "Hello in parallel1 stage1"
+                            }
+                        }
+                        stage ("parallel1 stage2") {
+                            steps {
+                                echo "Hello in parallel1 stage2"
+                            }
+                        }
+                    }
+                }
+
+                stage ("parallel2") {
+                    agent { label "build" }
+
+                    stages {
+                        stage ("parallel2stage1") {
+                            steps {
+                                echo "Hello in parallel2stage1"
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        stage ("Stage2") {
+          agent { label "build" }
+          steps {
+            echo "Hello in stage2"
+          }
+        }
+    }
+}
